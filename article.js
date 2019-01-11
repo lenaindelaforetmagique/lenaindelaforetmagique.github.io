77a3b43f6348a31b7ab365bdfe59a5fd641a0bd8
lenaindelaforetmagique <morin_xavier@hotmail.fr>
2019-01-12 00:38:27 +0100

    ajout des articles, première version lisible...

---
+++ article.js
@@ -0,0 +1,88 @@
+loadArticle = function(url) {
+  let dom = document.createElement('article');
+  let main = document.getElementById('main');
+  main.appendChild(dom);
+
+  let requ = new XMLHttpRequest();
+  requ.responseType = "json";
+  requ.onload = function() {
+    if (requ.response != null) {
+      let article = new Article(requ.response, dom);
+      article.show();
+    } else {
+      dom.innerHTML = "<p class=\"error\">L'article <em>" + url + "</em> n'a pas pu être chargé correctement...</p>";
+
+    }
+  };
+  requ.open('GET', url);
+  requ.send();
+}
+
+
+
+
+class Article {
+  constructor(data, dom_) {
+    this.dom = dom_;
+    this.title = data.title;
+    this.date = data.date;
+    this.author = data.author;
+    this.URL = data.URL;
+    this.imgURL = data.imgURL;
+    this.introduction = data.introduction;
+    this.content = data.content;
+    // Structure of longDescription
+    // for (let par of this.longDescription) {
+    //   console.log(par.title);
+    //   for (let line of par.lines) {
+    //     console.log("*" + line);
+    //   }
+    // }
+  }
+
+  show() {
+    let h2 = document.createElement('h2');
+    h2.innerHTML = this.title;
+    this.dom.appendChild(h2);
+
+    let date = document.createElement("div");
+    date.setAttribute("class", "date");
+    date.innerHTML = this.date;
+    this.dom.appendChild(date);
+
+    let a_link = document.createElement('a');
+    a_link.setAttribute('href', this.URL);
+    a_link.setAttribute('target', 'blank');
+
+    let img = document.createElement('img');
+    img.setAttribute("src", this.imgURL);
+    a_link.appendChild(img);
+    this.dom.appendChild(a_link);
+
+    let intro = document.createElement('p');
+    intro.innerHTML = this.introduction;
+    this.dom.appendChild(intro);
+
+    for (let para of this.content) {
+      if (para.title != "") {
+        let h3 = document.createElement('h3');
+        h3.innerHTML = para.title;
+        this.dom.appendChild(h3);
+      }
+
+      for (let line of para.lines) {
+        let domLine = document.createElement('p');
+        domLine.innerHTML = line;
+        this.dom.appendChild(domLine);
+      }
+
+    }
+    // let author = document.createElement("p");
+    // author.setAttribute("class", "author");
+    // author.innerHTML = this.author;
+    // this.dom.appendChild(author);
+
+  }
+
+
+}
--- script.js
+++ script.js
@@ -3,12 +3,8 @@ var request = new XMLHttpRequest();
 request.responseType = 'json';
 request.onload = function() {
   if (request.response != null) {
-    for (let url of request.response.list) {
-      let dom = document.createElement('article');
-      let main = document.getElementById('main');
-      main.appendChild(dom);
-      console.log(url);
-      loadArticle(url, dom);
+    for (let url of request.response.URLlist) {
+      loadArticle(url);
     }
   }
 }
@@ -21,38 +17,4 @@ function parseHTML(html) {
   var t = document.createElement('template');
   t.innerHTML = html;
   return t.content.cloneNode(true);
-}
-
-
-
-loadArticle = function(url, dom) {
-  let requ = new XMLHttpRequest();
-  requ.responseType = "json";
-  requ.onload = function() {
-    if (requ.response != null) {
-      let h2 = document.createElement('h2');
-      h2.innerHTML = requ.response.title;
-      dom.appendChild(h2);
-
-      let a_link = document.createElement('a');
-      a_link.setAttribute('href', requ.response.URL);
-      a_link.setAttribute('target', 'blank');
-
-      let img = document.createElement('img');
-      img.setAttribute("src", requ.response.imgURL);
-      a_link.appendChild(img);
-      dom.appendChild(a_link);
-
-      for (let line of requ.response.longDescription) {
-        let longDescr = document.createElement('p');
-        longDescr.innerHTML = line;
-        dom.appendChild(longDescr);
-      }
-
-    } else {
-      dom.innerHTML = "<p>L'article <em>" + url + "</em> est introuvable...</p>";
-    }
-  };
-  requ.open('GET', url);
-  requ.send();
 }
